b9bf5df0cb513b9f4f46df5e3efeb91a51cc6932
enjsite <enjyweb@gmail.com>
2019-12-09 15:30:08 +0300

    eureka

--- shop-eureka/src/test/java/ru/geekbrains/eureka/ShopEurekaApplicationTests.java
+++ shop-eureka/src/test/java/ru/geekbrains/eureka/ShopEurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.geekbrains.zuul;
+package ru.geekbrains.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class ZuulProxyApplicationTests {
+class ShopEurekaApplicationTests {
 
     //@Test
     void contextLoads() {
